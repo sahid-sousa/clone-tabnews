06a3fdf0b02e1755e0fa7ca8a9dbe668f7da41f5
Sahid Sousa <sahid@acception.com.br>
2024-11-22 16:36:15 -0300

    Ajustando tela inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
+import Image from "next/image";
+
 function Home() {
-    return <h1>Teste</h1>
+    return (
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center', // Centraliza horizontalmente
+          alignItems: 'center', // Centraliza verticalmente
+          height: '100vh' // Ocupa a altura total da tela
+        }}>
+          <div style={{
+            padding: '20px',
+            backgroundColor: '#f0f0f0',
+            borderRadius: '8px',
+            justifyContent: 'center'
+          }}>
+            <h1>Cadê o meu?</h1>
+            <Image
+                src="/pqto.gif"
+                alt="Meu Pqto"
+                width={1000}
+                height={500}
+            />
+          </div>
+        </div>
+      );
 }
 
 export default Home;
